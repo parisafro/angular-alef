d8b33e944159261d171a9a1eaa523045a1436066
parisafro <parisafro.pf@gmail.com>
2020-05-26 16:29:12 +0430

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-cwbgfy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-cwbgfy)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+ordersData = [
+{ id: 1, name: 'order 1' },
+{ id: 2, name: 'order 2' },
+{ id: 3, name: 'order 3' },
+{ id: 4, name: 'order 4' }
+];
+OnClick(form){
+    var order1 = form.order1;
+    console.log(order1);
+  
+    var order2 = form.order2;
+    console.log(order2);
+  
+    var order3= form.order3;
+    console.log(order3);
+
+    var order4= form.order4;
+    console.log(order4);
+
+}
+}
